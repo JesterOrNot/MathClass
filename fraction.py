ac4ade5523249d97dd263e2c3695013952e7513d
Sean Hellum <seanhellum45@gmail.com>
2020-01-14 14:37:58 +0000

    initialize `fraction.py`

---
+++ fraction.py
@@ -0,0 +1,23 @@
+"""File that holds fraction class."""
+
+
+class Fraction:
+    """Class for Python."""
+
+    def __init__(self, numerator=1, denominator=1):
+        """Defailt constructor for Fraction."""
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __repr__(self):
+        """Str Representation of fraction."""
+        return self.numerator + "/" + self.denominator
+
+    def __str__(self):
+        """Str representation of fraction."""
+        return self.numerator + "/" + self.denominator
+
+    @classmethod
+    def from_int(cls, number):
+        """Take a int and return a fraction."""
+        return Fraction(number, 1)
